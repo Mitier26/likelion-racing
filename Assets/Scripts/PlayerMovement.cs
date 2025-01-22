d704ab99775c77779a946e6bf0d81b857dd950c7
Molkang <mitier26@gmail.com>
2025-01-22 15:41:30 +0900

    fix 이동

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,5 +21,8 @@ public class PlayerMovement : MonoBehaviour
         float h  = Input.GetAxis("Horizontal");
         
         rb.velocity = Vector3.right * (h * moveSpeed);
+        
+        float clampedX = Mathf.Clamp(transform.position.x, -5, 5);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
     }
 }
